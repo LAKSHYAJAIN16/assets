b4beb54963bf666edc4fc84ec28f1ba72a425a36
LAKSHYAJAIN16 <81555595+LAKSHYAJAIN16@users.noreply.github.com>
2021-11-01 17:44:47 +0530

    Update dat.go

--- dat.go
+++ dat.go
@@ -4,4 +4,5 @@ func main() {
     fmt.Println("dat");
     fmt.Println("dat setup 2" + (10+20));
     fmt.Println("dat setup 3" + ("str" + "ops));
+    fmt.Println("dat setup 4" + ("str" + "ops));
 }
