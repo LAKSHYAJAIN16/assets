9355cfe95a9d88ab1cf3895607e043b0608a218c
LAKSHYAJAIN16 <81555595+LAKSHYAJAIN16@users.noreply.github.com>
2021-11-01 17:45:01 +0530

    Update dat.go

--- dat.go
+++ dat.go
@@ -3,6 +3,6 @@ import "fmt"
 func main() {
     fmt.Println("dat");
     fmt.Println("dat setup 2" + (10+20));
-    fmt.Println("dat setup 3" + ("str" + "ops));
-    fmt.Println("dat setup 4" + ("str" + "ops));
+    fmt.Println("dat setup 3" + ("str" + "ops"));
+    fmt.Println("dat setup 4" + ("str" + "ops"));
 }
